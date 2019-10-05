233daaacbd17115d36e055a09be34410b0e9ae3b
Rogério Júnior <junior.rogerio8@gmail.com>
2019-10-05 18:29:11 -0300

    pax-app/Wiki#160 Generate the models
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
-import Sequelize from 'sequelize'
-import SequelizeConfig from '../config/mysql'
+import Sequelize from 'sequelize';
+import SequelizeConfig from '../config/mysql';
 
+import Chat from '../app/models/Chat';
+import Message from '../app/models/Message';
+
+const models = [Chat, Message];
 class Database {
   constructor() {
     this.init();
@@ -8,6 +12,9 @@ class Database {
 
   init() {
     this.connection = new Sequelize(SequelizeConfig);
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
