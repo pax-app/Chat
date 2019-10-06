1c931d552f93b71f14c87d0b79c078222113a918
Rogério Júnior <junior.rogerio8@gmail.com>
2019-10-06 03:06:18 -0300

    pax-app/Wiki#160 Add chat schema validators
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

---
+++ src/app/validators/ChatList.js
@@ -0,0 +1,17 @@
+import * as yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    const schema = yup.object().shape({
+      user_id: yup.number().notRequired(),
+      provider_id: yup.number().notRequired(),
+    });
+
+    await schema.validate(req.body, { abortEarly: false });
+    return next();
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: 'Validation fails', messages: error.inner });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import ChatController from './app/controllers/ChatController';
 import MessageController from './app/controllers/MessageController';
 
+import validateChatList from './app/validators/ChatList';
+import validateChatStore from './app/validators/ChatStore';
+
 const routes = new Router();
 
-routes.get('/chats', ChatController.index);
-routes.post('/chats', ChatController.create);
+routes.get('/chats', validateChatList, ChatController.index);
+routes.post('/chats', validateChatStore, ChatController.create);
 routes.delete('/chats/:chat_id', ChatController.destroy);
 
 routes.get('/messages', MessageController.index);
