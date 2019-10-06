929fa2450e21d8f2e8b85981596398e8741dd615
Rogério Júnior <junior.rogerio8@gmail.com>
2019-10-06 03:36:49 -0300

    pax-app/Wiki#160 Message schema validation
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import MessageController from './app/controllers/MessageController';
 
 import validateChatList from './app/validators/ChatList';
 import validateChatStore from './app/validators/ChatStore';
+import validateMessageList from './app/validators/MessageList';
+import validateMessageStore from './app/validators/MessageStore';
 
 const routes = new Router();
 
@@ -11,7 +13,7 @@ routes.get('/chats', validateChatList, ChatController.index);
 routes.post('/chats', validateChatStore, ChatController.create);
 routes.delete('/chats/:chat_id', ChatController.destroy);
 
-routes.get('/messages', MessageController.index);
-routes.post('/messages', MessageController.create);
+routes.get('/messages', validateMessageList, MessageController.index);
+routes.post('/messages', validateMessageStore, MessageController.create);
 
 export default routes;
