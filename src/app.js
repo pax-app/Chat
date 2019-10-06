f9cb85ed8eca59f307815b92b4dcbdf550b7c9f8
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-10-06 15:30:31 -0300

    pax-app/Wiki#160 Connecting to Sentry for monitoring the app
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Rogério Júnior <junior.rogerio8@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import path from 'path';
 import http from 'http';
 import express from 'express';
 import socketio from 'socket.io';
+
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+
 import routes from './routes';
 import sockets from './sockets';
 import './database';
@@ -10,12 +14,17 @@ const app = express();
 const raw_server = http.createServer(app);
 const io = socketio(raw_server);
 
+Sentry.init(sentryConfig);
+app.use(Sentry.Handlers.requestHandler());
+
 const publicDir = path.join(__dirname, '../public');
 app.use(express.static(publicDir));
 
 app.use(express.json());
 app.use(routes);
 sockets(io);
+
+app.use(Sentry.Handlers.errorHandler());
 
 const port = process.env.PORT || 3001;
 raw_server.listen(port, () => console.log(`Server running at port ${port}`));
