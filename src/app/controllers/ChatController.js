43c09e7c620059930dd50e1367670bb1329fd86d
Rogério Júnior <junior.rogerio8@gmail.com>
2019-10-05 23:54:36 -0300

    pax-app/Wiki#160 Chat controller index lists all chats from a user or provider
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/app/controllers/ChatController.js
+++ src/app/controllers/ChatController.js
@@ -3,8 +3,15 @@ import express from 'express';
 
 class ChatController {
   async index(req, res) {
-    return res.json({ ok: true });
+    const { user_id, provider_id } = req.body;
+
+    const chats = await Chat.findAll({
+      where: user_id ? { user_id } : { provider_id },
+    });
+
+    return res.json(chats);
   }
+
   async create(req, res) {
     const { user_id, provider_id } = req.body;
 
