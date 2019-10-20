7450c0436ccabef9b4b5f2e4837b1c004b07b56c
Rogério Júnior <junior.rogerio8@gmail.com>
2019-10-20 19:59:23 -0300

    pax-app/Wiki#160 Removing everything related to websocket io
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
    Co-authored-by: Marcos Nery <marcosnery.comp@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,30 +1,20 @@
-import path from 'path';
-import http from 'http';
 import Youch from 'youch';
 import express from 'express';
-import socketio from 'socket.io';
 import 'express-async-errors';
 
 import * as Sentry from '@sentry/node';
 import sentryConfig from './config/sentry';
 
 import routes from './routes';
-import sockets from './sockets';
 import './database';
 
 const app = express();
-const raw_server = http.createServer(app);
-const io = socketio(raw_server);
 
 Sentry.init(sentryConfig);
 app.use(Sentry.Handlers.requestHandler());
 
-const publicDir = path.join(__dirname, '../public');
-app.use(express.static(publicDir));
-
 app.use(express.json());
 app.use(routes);
-sockets(io);
 app.use(Sentry.Handlers.errorHandler());
 
 const node_env = process.env.NODE_ENV || 'development';
@@ -38,4 +28,4 @@ app.use(async (error, req, res, next) => {
 });
 
 const port = process.env.PORT || 3001;
-raw_server.listen(port, () => console.log(`Server running at port ${port}`));
+app.listen(port, () => console.log(`Server running at port ${port}`));
